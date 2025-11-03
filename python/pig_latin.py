d5ea1f502035f1c445a01d9be02f35b973e9ae10
duy4nt <singhduyant@gmail.com>
2025-11-03 17:38:02 +0000

    Completed the pig latin program

--- python/pig_latin.py
+++ python/pig_latin.py
@@ -15,3 +15,33 @@ for word in text.split():
     if len(word) == 0:
         pig_latin.append(prefix_non_letters)
         continue
+
+    suffix_non_letters = ''
+
+    while not word[-1].isalpha():
+        suffix_non_letters = word[-1] + suffix_non_letters
+        word = word[:-1]
+
+    was_upper = word.isupper()
+    was_title = word.istitle()
+
+    word = word.lower()
+
+    prefix_consonanats = ''
+    while len(word) > 0 and not word[0] in VOWELS:
+        prefix_consonants += word[0]
+        word = word[1:]
+
+    if perfix_consonants != '':
+        word += prefix_consonants + 'ay'
+    else:
+        word += 'yay'
+
+    if was_upper:
+        word = word.upper()
+    if was_titel:
+        word = word.title()
+
+    pig_latin.append(prefix_non_letters + word + suffix_non_letters)
+
+    print(' '.join(pig_latin))
