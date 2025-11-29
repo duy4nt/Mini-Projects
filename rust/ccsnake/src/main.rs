d0276767b6c7a9db1b844c09bc9f742b3322b5c7
duy4nt <singhduyant@gmail.com>
2025-11-29 07:11:27 +0000

    Added the variable snek size

--- rust/ccsnake/src/main.rs
+++ rust/ccsnake/src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{picking::window, prelude::*, window::PrimaryWindow};
 
 #[derive(Component)]
 struct SnakeHead;
@@ -27,16 +27,6 @@ impl Size {
 const ARENA_WIDTH: u32 = 10;
 const ARENA_HEIGHT: u32 = 10;
 const SNAKE_HEAD_COLOR: Color = Color::srgb(0.7, 0.7, 0.7);
-
-fn main() {
-    println!("Hello, world!");
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup_camera)
-        .add_systems(Startup, spawn_snake)
-        .add_systems(Update, snake_movement)
-        .run();
-}
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2d);
@@ -76,4 +66,52 @@ fn snake_movement(
             transform.translation.y -= 2.0;
         }
     }
+}
+
+fn size_scaling(
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    mut q: Query<(&Size, &mut Transform)>,
+) {
+    let Ok(window) = window_query.single() else {
+        return;
+    };
+    for (sprite_size, mut transform) in q.iter_mut() {
+        transform.scale = Vec3::new(
+            sprite_size.width / ARENA_WIDTH as f32 * window.width(),
+            sprite_size.height / ARENA_HEIGHT as f32 * window.height(),
+            1.0,
+        );
+    }
+}
+
+fn position_translation(
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    mut q: Query<(&Position, &mut Transform)>,
+) {
+    fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
+        let title_size = bound_window / bound_game;
+        pos / bound_game * bound_window - (bound_window / 2.0) + (title_size / 2.0)
+    }
+    let Ok(window) = window_query.single() else {
+        return;
+    };
+
+    for (pos, mut transform) in q.iter_mut() {
+        transform.translation = Vec3::new(
+            convert(pos.x as f32, window.width(), ARENA_WIDTH as f32),
+            convert(pos.y as f32, window.height(), ARENA_HEIGHT as f32),
+            0.0,
+        );
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup_camera)
+        .add_systems(Startup, spawn_snake)
+        .add_systems(Update, snake_movement)
+        .add_systems(PostUpdate, (position_translation, size_scaling))
+        .run();
 }
