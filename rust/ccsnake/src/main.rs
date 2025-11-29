2ad5d655d05eafd4ffc786ae952290699ce28549
duy4nt <singhduyant@gmail.com>
2025-11-29 06:31:08 +0000

    added the movement feature

--- rust/ccsnake/src/main.rs
+++ rust/ccsnake/src/main.rs
@@ -3,6 +3,29 @@ use bevy::prelude::*;
 #[derive(Component)]
 struct SnakeHead;
 
+#[derive(Component, Clone, Copy, PartialEq, Eq)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Size {
+    width: f32,
+    height: f32,
+}
+
+impl Size {
+    pub fn square(x: f32) -> Self {
+        Self {
+            width: x,
+            height: x,
+        }
+    }
+}
+
+const ARENA_WIDTH: u32 = 10;
+const ARENA_HEIGHT: u32 = 10;
 const SNAKE_HEAD_COLOR: Color = Color::srgb(0.7, 0.7, 0.7);
 
 fn main() {
@@ -10,9 +33,47 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup_camera)
+        .add_systems(Startup, spawn_snake)
+        .add_systems(Update, snake_movement)
         .run();
 }
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2d);
+}
+
+fn spawn_snake(mut commands: Commands) {
+    commands
+        .spawn((
+            Sprite {
+                color: SNAKE_HEAD_COLOR,
+                custom_size: Some(Vec2::new(10.0, 10.0)),
+                ..default()
+            },
+            Transform::default(),
+            SnakeHead,
+        ))
+        .insert(SnakeHead)
+        .insert(Position { x: 3, y: 3 })
+        .insert(Size::square(0.8));
+}
+
+fn snake_movement(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut head_positions: Query<&mut Transform, With<SnakeHead>>,
+) {
+    for mut transform in head_positions.iter_mut() {
+        if keyboard_input.pressed(KeyCode::ArrowLeft) {
+            transform.translation.x -= 2.0;
+        }
+        if keyboard_input.pressed(KeyCode::ArrowRight) {
+            transform.translation.x += 2.0;
+        }
+        if keyboard_input.pressed(KeyCode::ArrowUp) {
+            transform.translation.y += 2.0;
+        }
+        if keyboard_input.pressed(KeyCode::ArrowDown) {
+            transform.translation.y -= 2.0;
+        }
+    }
 }
